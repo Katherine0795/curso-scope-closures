f86e1c3c5462d61322400cbac4b2701f89b42fb5
katherine <katherine.marquinam@gmail.com>
2025-06-12 07:03:23 -0500

    re var

---
+++ src/scope/re.js
@@ -0,0 +1,11 @@
+var firstName;  //Valor indefinico
+firstName = "John"; //Valor definido
+console.log(firstName); // Imprime "John"
+
+var lastName = "Doe"; // Declaramos y asignamos
+lastName = "Smith"; // Reasignamos
+console.log(lastName); // Imprime "Doe"
+
+var secondName ="David"; // Declaramos y asignamos
+var secondName = "Ana"; // Redeclaramos y reasignamos
+console.log(secondName); // Imprime "Ana"
