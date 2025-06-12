a7ada4676cf645ecdf4b049f38edc2876e63eff4
katherine <katherine.marquinam@gmail.com>
2025-06-12 04:51:14 -0500

    Global scope (forma01)

--- src/scope/global.js
+++ src/scope/global.js
@@ -5,3 +5,15 @@ var b = "b"; //declarando / asignamos
 b = "bb"; // reasignamos
 var a = "aa"; // redeclaramos
 
+// Global scope
+var fruit = "apple"; // variable global
+console.log(fruit);   
+
+//Funcion para leer variable global
+function bestFruit() {
+  console.log(fruit);      
+}
+
+//Ejecutamos la funcion
+bestFruit(); // apple
+
