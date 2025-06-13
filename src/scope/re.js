35c394d1c22644a00e747f02e221eb7f84c30573
katherine <katherine.marquinam@gmail.com>
2025-06-13 01:08:22 -0500

    re let

--- src/scope/re.js
+++ src/scope/re.js
@@ -9,3 +9,9 @@ console.log(lastName); // Imprime "Doe"
 var secondName ="David"; // Declaramos y asignamos
 var secondName = "Ana"; // Redeclaramos y reasignamos
 console.log(secondName); // Imprime "Ana"
+
+//Let
+ let fruit = "Apple"; // Declaramos y asignamos
+fruit = "Banana"; // Reasignamos
+
+let fruit = "Orange"; // Esto causará un error porque no se puede redeclarar una variable con let en el mismo ámbito
