f325e30a9c7244bdd862d921c56cc442925b4c3a
katherine <katherine.marquinam@gmail.com>
2025-06-13 01:17:24 -0500

    re const

--- src/scope/re.js
+++ src/scope/re.js
@@ -15,3 +15,16 @@ console.log(secondName); // Imprime "Ana"
 fruit = "Banana"; // Reasignamos
 
 let fruit = "Orange"; // Esto causará un error porque no se puede redeclarar una variable con let en el mismo ámbito
+
+// const
+const animal = "Dog"; // Declaramos y asignamos
+animal = "Cat"; // Esto causará un error porque no se puede reasignar una variable const
+const animal = "Fish"; // Esto causará un error porque no se puede redeclarar una variable const en el mismo ámbito
+console.log(animal); // Imprime "Dog"
+
+const vehicles = []; // Declaramos un array vacío
+vehicles.push("🚙")
+console.log(vehicles);
+
+vehicles.pop(); // Eliminamos el último elemento del array
+console.log(vehicles); // Imprime "[]"
